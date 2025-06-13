85ab66e83c19d21e8d65a089b19f2fa6605ecf62
EilerGomez <gomezeiler250@gmail.com>
2025-06-13 01:15:12 -0600

    Haciendo responsiva algunas cosas

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 8080;
 app.use(express.static(path.join(__dirname, 'dist/frontendcafeteria')));
 
 // Para cualquier otra ruta, enviar index.html
-app.get('/*', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/frontendcafeteria/index.html'));
 });
 
