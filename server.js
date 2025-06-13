2c1811dd1d9701ccd40c33dba80bc6388e39732a
EilerGomez <gomezeiler250@gmail.com>
2025-06-13 00:34:08 -0600

    Agregar server.js para subirlo a un web service1

--- server.js
+++ server.js
@@ -1,18 +1,15 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
+const PORT = process.env.PORT || 8080;
 
-// Ruta absoluta a tu carpeta de distribución
-const distPath = path.join(__dirname, 'dist', 'frontendcafeteria');
+app.use(express.static(path.join(__dirname, 'dist/frontendcafeteria')));
 
-app.use(express.static(distPath));
-
-// Para rutas internas, redirige siempre a index.html
 app.get('*', (req, res) => {
-  res.sendFile(path.join(distPath, 'index.html'));
+  res.sendFile(path.join(__dirname, 'dist/frontendcafeteria/index.html'));
 });
 
-const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en puerto ${PORT}`);
+  console.log(`Servidor Angular corriendo en http://localhost:${PORT}`);
 });
